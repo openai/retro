d7c6207cbc975b311ac58989b2c97f3fab7d9f67
Vicki Pfau <vickipfau@openai.com>
2018-01-11 09:38:02 -0800

    src: Migrate cores into movable directory

--- src/coreinfo.cpp
+++ src/coreinfo.cpp
@@ -2,6 +2,7 @@
 
 #include "coreinfo.h"
 #include "data.h"
+#include "utils.h"
 
 using namespace std;
 using nlohmann::json;
@@ -21,7 +22,7 @@ string corePath(const string& hint) {
 	if (envDir) {
 		s_coreDirectory = envDir;
 	} else if (hint.size()) {
-		s_coreDirectory = hint;
+		s_coreDirectory =  drillUp({ "cores" }, ".", hint);
 	} else {
 		s_coreDirectory = ".";
 	}
