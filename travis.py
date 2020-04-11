3a7f5579094be65fa00767ced2c80e85da7c08b6
Christopher Hesse <christopherhesse@users.noreply.github.com>
2020-04-10 20:40:49 -0700

    .

--- travis.py
+++ travis.py
@@ -67,8 +67,9 @@ def main():
         if os_name == 'osx':
             cmake_options = ['-DCMAKE_PREFIX_PATH=/usr/local/opt/qt', '-DBUILD_UI=ON']
         elif os_name == 'linux':
+            include_suffix = "m" if float(os.environ['PYVER']) < 3.8 else ""
             cmake_options = ['-DBUILD_MANYLINUX=ON',
-                             '-DPYTHON_INCLUDE_DIR=%s/include/python%sm' % (sys.base_prefix, os.environ['PYVER'])]
+                             '-DPYTHON_INCLUDE_DIR=%s/include/python%s%s' % (sys.base_prefix, os.environ['PYVER'], include_suffix)]
             if cross in ('win32', 'win64'):
                 cmake_options = ['-DCMAKE_TOOLCHAIN_FILE=docker/cmake/%s.cmake' % cross, '-DBUILD_UI=ON']
             if cross == 'win32':
